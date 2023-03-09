275d6cafa283adab5982735a60c9e3da0b75010f
Pramana Yuda Sayeti <pramanayuda772@gmail.com>
2023-03-09 14:28:39 +0700

    push

---
+++ resources/views/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css">
+    <style>
+        #footer {
+            position: fixed !important;
+            bottom: 0 !important;
+        }
+    </style>
+
+    <title>SIAPDI KEMENAG - Sistem Informasi Agenda Pimpinan</title>
+
+    <script src="https://upload-widget.cloudinary.com/global/all.js" type="text/javascript"></script>
+</head>
+
+{{-- <body class="hold-transition sidebar-mini"> --}}
+
+{{-- <body class="layout-top-nav"> --}}
+
+<body>
+
+    <div class="wrapper" id="app">
+
+    </div>
+
+    <!-- REQUIRED SCRIPTS -->
+    <script src="https://widget.cloudinary.com/v2.0/global/all.js" type="text/javascript"></script>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+
+</body>
+
+</html>
